0d19a3ecb4676b2c28644e7859b35c654e471444
nawe12 <nahuelmartinez880@gmail.com>
2024-01-24 23:55:23 -0300

    Primera Preentrega - Martinez

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemListContainer";
+import Footer from "./components/Footer";
+
+function App() {
+  const [allProducts, setAllProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
+
+  return (
+    <>
+      <NavBar
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+      <ItemListContainer
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
